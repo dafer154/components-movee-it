a9aaba6f4b5af9fc2c96edfc4c4f5c1272c79823
David Zuluaga <david_fza@hotmail.com>
2020-01-22 22:38:05 -0500

    Advance in component Input Number

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 // import Button from "./components/Button/Button";
 import InputNumber from "./components/InputNumber/InputNumber";
-import InputText from "./components/InputText/InputText";
+// import InputText from "./components/InputText/InputText";
 import SelectInput from "./components/SelectInput/SelectInput";
 import Autocomplete from "./components/Autocomplete/Autocomplete";
 import WrapperButtons from "./components/Button/WrapperButtons";
 import WrapperInputText from "./components/InputText/WrapperInputText";
+import WrapperInputNumber from "./components/InputNumber/WapperInputNumber";
 
 
 class App extends Component {
@@ -21,7 +22,8 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           {/* <Route path="/button" component={Button} /> */}
           <Route path="/button" component={WrapperButtons} />
-          <Route path="/input-number" component={InputNumber} />
+          {/* <Route path="/input-number" component={InputNumber} /> */}
+          <Route path="/input-number" component={WrapperInputNumber} />
           {/* <Route path="/input-text" component={InputText} /> */}
           <Route path="/input-text" component={WrapperInputText} />
           <Route path="/select-input" component={SelectInput} />
