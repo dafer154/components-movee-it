80d83a51d81cb0f38e2415da445d56184ed79b01
David Zuluaga <david_fza@hotmail.com>
2020-01-29 11:19:56 -0500

    Updates

---
+++ precache-manifest.d54a6270014c38912b38835407486d21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e8edbcb088569727b449be135a697b",
+    "url": "/components-movee-it/index.html"
+  },
+  {
+    "revision": "560e473a5bc56f424f28",
+    "url": "/components-movee-it/static/css/main.3ecbf0d6.chunk.css"
+  },
+  {
+    "revision": "a19400a459943ac078b1",
+    "url": "/components-movee-it/static/js/2.91d812e8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/components-movee-it/static/js/2.91d812e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "560e473a5bc56f424f28",
+    "url": "/components-movee-it/static/js/main.50e202f9.chunk.js"
+  },
+  {
+    "revision": "9efe706fe8b63937b247",
+    "url": "/components-movee-it/static/js/runtime-main.89140a4d.js"
+  }
+]);
