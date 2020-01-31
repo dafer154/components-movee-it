8197c4d8ca16884c575a419ff368159e253d6640
David Zuluaga <david_fza@hotmail.com>
2020-01-31 11:54:40 -0500

    Updates

---
+++ precache-manifest.14a58e5210a2af07a165acbce06028c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb2c977fa1640285a709ed843c8cc10",
+    "url": "/components-movee-it/index.html"
+  },
+  {
+    "revision": "49e47925e435c480ec30",
+    "url": "/components-movee-it/static/css/main.13d60236.chunk.css"
+  },
+  {
+    "revision": "72b4abebb928fcf1dd50",
+    "url": "/components-movee-it/static/js/2.5206eca4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/components-movee-it/static/js/2.5206eca4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49e47925e435c480ec30",
+    "url": "/components-movee-it/static/js/main.e4169477.chunk.js"
+  },
+  {
+    "revision": "9efe706fe8b63937b247",
+    "url": "/components-movee-it/static/js/runtime-main.89140a4d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/components-movee-it/precache-manifest.939824fa00cc33bec6080a784d63b4b0.js"
+  "/components-movee-it/precache-manifest.14a58e5210a2af07a165acbce06028c4.js"
 );
 
 self.addEventListener('message', (event) => {
